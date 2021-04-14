441717257d3c7b0b047d34923877d6be80d260aa
Samuel Gonzalez <xame2005@gmail.com>
2021-04-14 01:20:37 -0500

    [ADDED] methods

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template lang="pug">
   <div id="app">
-    <p v-model="showValue">{{value}}</p>
-    <p v-if="showValue">{{value}}</p>
-    <p v-else>{{'Algo más'}}</p>
-    <p v-else-if="true">{{'Algo mas'}}</p>
-    <ul>
-      <li v-for="i in items">{{i}}</li>
-    </ul>
+    <input v-model="name">
+    <button v-on:click="format">Format</button>
+    <h1>{{ formatName }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
+
   data() {
     return {
-      msg: "Welcome to Your Vue.js App",
-      showValue: false,
-      value: "Algo",
-      items: [1, 2, 3, 4, 5]
+      name: "",
+      formatName: ""
     };
+  },
+  methods: {
+    format() {
+      this.formatName = this.name
+        .split(" ")
+        .join("-")
+        .toUpperCase();
+    }
   }
 };
 </script>
