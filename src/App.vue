0be8ba4712f01cc30bcbd6217ddece8a5f35088e
Samuel Gonzalez <xame2005@gmail.com>
2021-04-14 00:10:23 -0500

    [CREATED] interpolations and directives

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  <div id="app">
+    <p v-model="showValue">{{value}}</p>
+    <p v-if="showValue">{{value}}</p>
+    <p v-else>{{'Algo más'}}</p>
+    <p v-else-if="true">{{'Algo mas'}}</p>
+    <ul>
+      <li v-for="i in items">{{i}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      showValue: false,
+      value: "Algo",
+      items: [1, 2, 3, 4, 5]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+</style>
