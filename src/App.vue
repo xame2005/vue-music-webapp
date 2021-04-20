27703b5f27c9e6bf2ab444b6c7c40d6fddb41612
Samuel Gonzalez <xame2005@gmail.com>
2021-04-19 23:21:46 -0500

    [CREATED] WebApp NavBar and search input

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template lang="pug">
-  <div id="app">
-    <input v-model="name">
-    <button v-on:click="format">Format</button>
-    <h1>{{ formatName }}</h1>
-  </div>
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="Buscar Canciones", v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{searchMessage}}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{t.name}} - {{t.artist}}
 </template>
 
 <script>
+const tracks = [
+  { name: "Cancion 1", artist: "Artista 1" },
+  { name: "Cancion 2", artist: "Artista 2" },
+  { name: "Cancion 3", artist: "Artista 3" }
+];
 export default {
   name: "app",
 
   data() {
     return {
-      name: "",
-      formatName: ""
+      searchQuery: "",
+      tracks: []
     };
   },
+
+  computed: {
+    searchMessage() {
+      return `Encontrados: ${this.tracks.length}`;
+    }
+  },
   methods: {
-    format() {
-      this.formatName = this.name
-        .split(" ")
-        .join("-")
-        .toUpperCase();
+    search() {
+      this.tracks = tracks;
     }
   }
 };
@@ -29,4 +45,8 @@ export default {
 
 <style lang="scss">
 @import "./scss/main.scss";
+
+.results {
+  margin-top: 50px;
+}
 </style>
